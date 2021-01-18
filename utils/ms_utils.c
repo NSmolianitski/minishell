988ad807dbfafb30df54b8abaf4d4de72eeebef6
pkentaur <n.smolianitski@gmail.com>
2021-01-18 16:47:33 +0300

    PWD, ms_strlen, print_line functions added

---
+++ utils/ms_utils.c
@@ -0,0 +1,19 @@
+#include "unistd.h"
+
+int 	ms_strlen(const char *str)
+{
+	int 	i;
+
+	i = 0;
+	while (str[i])
+		++i;
+	return (i);
+}
+
+int		print_line(char const *line)
+{
+	if (line == NULL)
+		return (-1);
+	write(1, line, ms_strlen(line));
+	return (0);
+}
