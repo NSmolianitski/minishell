9df012c987e6b5f6b4eb765755d081847e77ba2a
pkentaur <n.smolianitski@gmail.com>
2021-01-18 19:21:09 +0300

    Some prototype of shell prompt added

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <unistd.h>
 #include "ms_commands.h"
 
-int		main(void)
+static void	print_shell_tag(void)
 {
+	write(1, "minishell$ ", 11);
+}
+
+int			main(void)
+{
+	char	buff[200];			//Don't forget to create a string from read with malloc (or create custom realloc)
+	int		read_symbols;
+
+	while (1)
+	{
+		print_shell_tag();
+		read_symbols = read(0, buff, 200);
+		buff[read_symbols] = '\0';
+		printf("%s", buff);
+	}
 	return (0);
 }
