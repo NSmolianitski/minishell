e2974412ef1cefaaacebbb8dc341949e74e53bad
pkentaur <n.smolianitski@gmail.com>
2021-01-20 15:07:56 +0300

    shell-tag color, get_cmd_line and echo function added

--- main.c
+++ main.c
@@ -1,22 +1,46 @@
 #include <unistd.h>
-#include "ms_commands.h"
+#include <stdlib.h>
+#include "libft.h"
 
 static void	print_shell_tag(void)
 {
+	write(1, "\033[32m", ft_strlen("\033[32m")); //output color
 	write(1, "minishell$ ", 11);
+	write(1, "\033[0m", ft_strlen("\033[0m"));
+}
+
+static char	*get_cmd_line(void)
+{
+	char	buff[1000];
+	char 	*cmd_line;
+	char 	*tmp;
+	int		read_bytes;
+
+	cmd_line = ft_strdup("");
+	while ((read_bytes = read(0, buff, 999)))
+	{
+		if (buff[read_bytes - 1] == '\n')
+			buff[read_bytes - 1] = '\0';
+		tmp = cmd_line;
+		cmd_line = ft_strjoin(cmd_line, buff);
+		free(tmp);
+		if (buff[read_bytes - 1] == '\0')
+			break ;
+	}
+	return (cmd_line);
 }
 
 int			main(void)
 {
-	char	buff[200];			//Don't forget to create a string from read with malloc (or create custom realloc)
-	int		read_symbols;
+	char	*cmd_line;
 
 	while (1)
 	{
 		print_shell_tag();
-		read_symbols = read(0, buff, 200);
-		buff[read_symbols] = '\0';
-		printf("%s", buff);
+		cmd_line = get_cmd_line();
+//		write(0, cmd_line, ft_strlen(cmd_line));  //print command line
+//		write(0, "\n", 1);
+		free(cmd_line);
 	}
 	return (0);
 }
